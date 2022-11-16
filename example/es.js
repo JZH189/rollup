71d7d11618d4af2de0c9b86ad920a6e7de68777f
victor jiang <vn544q7@email.wal-mart.com>
2022-11-16 22:57:46 +0800

    ast.bind方法

--- example/es.js
+++ example/es.js
@@ -1,24 +1,11 @@
 const name = 'victor jiang';
-const age = 17;
 
 function foo() {
 	console.log(123);
 	return 'foo';
 }
 
-var user = /*#__PURE__*/Object.freeze({
-	__proto__: null,
-	name: name,
-	age: age,
-	foo: foo
-});
-
 const fname = foo();
-let helloKety = async () => {
-	const { name } = await Promise.resolve().then(function () { return user; });
-	console.log('hello: ', name);
-};
-helloKety();
 {
 	console.log('这段代码保留');
 }
--- example/rollup.js
+++ example/rollup.js
@@ -1,8 +1,7 @@
 const { rollup } = require('../dist/rollup'); //注意稍后我们需要运行在node环境中，所以使用了require,而不是 import 方式
 //打包输入配置只给一个入口信息
 const inputOption = {
-	input: 'example/index.js',
-	perf: true
+	input: 'example/index.js'
 };
 //打包输出配置信息就简单点，分别定义输出的js文件格式还有文件名称
 const outputOption = {
