400808a6a53dd53ee9b7352718f729fcde4d0b2e
victorjiang <victorjiang189@163.com>
2022-12-10 02:15:20 +0800

    doc: 打包配置

--- example/rollup.js
+++ example/rollup.js
@@ -1,15 +1,13 @@
 const { rollup } = require('../dist/rollup'); //注意稍后我们需要运行在node环境中，所以使用了require,而不是 import 方式
-//打包输入配置只给一个入口信息
+//打包输入配置只给一个入口信息import { entry } from '../test/function/samples/duplicate-input-entry/entry';
+
 const inputOption = {
 	input: 'example/index.js'
 };
 //打包输出配置信息就简单点，分别定义输出的js文件格式还有文件名称
 const outputOption = {
 	file: 'example/es.js',
-	format: 'es',
-	manualChunks: {
-		isReference: ['is-reference']
-	}
+	format: 'es'
 };
 
 async function build() {
